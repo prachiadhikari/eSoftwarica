4fe34fbd78d633ad1aca126881b509d9193774e0
prachiadhikari <prachiadhikari22@gmail.com>
2019-12-25 09:11:48 +0545

    Initial commit

---
+++ app/src/main/java/com/prachi/esoftwarica/ui/dashboard/DashboardFragment.java
@@ -0,0 +1,120 @@
+package com.prachi.esoftwarica.ui.dashboard;
+
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.prachi.esoftwarica.Contacts;
+import com.prachi.esoftwarica.MainActivity;
+import com.prachi.esoftwarica.R;
+
+public class DashboardFragment extends Fragment implements RadioGroup.OnCheckedChangeListener,View.OnClickListener {
+
+    private DashboardViewModel dashboardViewModel;
+    RadioButton rdomale,rdofemale,rdoothers;
+    RadioGroup rdogroup;
+    EditText etName,etAddress,etAge;
+    Button btnsave;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        dashboardViewModel =
+                ViewModelProviders.of(this).get(DashboardViewModel.class);
+        View root = inflater.inflate(R.layout.addstudent_layout, container, false);
+
+
+        rdogroup=root.findViewById(R.id.rdoGroup);
+        etName=root.findViewById(R.id.etName);
+        etAddress=root.findViewById(R.id.etAddress);
+        etAge=root.findViewById(R.id.etAge);
+        btnsave=root.findViewById(R.id.btnSave);
+
+        rdogroup.setOnCheckedChangeListener( this);
+        btnsave.setOnClickListener(this);
+
+
+
+        final TextView textView = root.findViewById(R.id.text_dashboard);
+        dashboardViewModel.getText().observe(this, new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textView.setText(s);
+            }
+        });
+
+        return root;
+    }
+
+    String fullname,address,age,gender;
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId()==R.id.btnSave){
+            Toast.makeText(getActivity(),"save",Toast.LENGTH_SHORT).show();
+            fullname=etName.getText().toString();
+            age=etAge.getText().toString();
+            address=etAddress.getText().toString();
+
+            if(validate()){
+               MainActivity.contact.add(new Contacts(fullname,address,gender,Integer.parseInt(age)));
+               Toast.makeText(getContext(),"Student added",Toast.LENGTH_SHORT).show();
+            }
+        }
+
+
+
+    }
+    private  boolean validate(){
+        if(TextUtils.isEmpty(etName.getText())){
+            etName.setError("please enter full name");
+            etName.requestFocus();
+            return false;
+        }
+        else if(TextUtils.isEmpty(etAddress.getText())){
+            etAddress.setError("please enter address");
+            etAddress.requestFocus();
+            return false;
+        }
+        else if(TextUtils.isEmpty(etAge.getText())){
+            etAge.setError("please enter age");
+            etAge.requestFocus();
+            return false;
+        }
+
+        else if(TextUtils.isEmpty(gender)){
+            Toast.makeText(getContext(),"please select gender",Toast.LENGTH_SHORT).show();
+            return  false;
+        }
+        return true;
+    }
+
+    @Override
+    public void onCheckedChanged(RadioGroup group, int checkedId) {
+
+
+        if(checkedId==R.id.rdobtnMale){
+            gender="male";
+        }
+        if(checkedId==R.id.rdobtnFemale){
+            gender="female";
+        }
+        if(checkedId==R.id.rdobtnOthers){
+            gender="other";
+        }
+
+    }
+}
---
+++ app/src/main/java/com/prachi/esoftwarica/ui/home/HomeFragment.java
@@ -0,0 +1,69 @@
+package com.prachi.esoftwarica.ui.home;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.prachi.esoftwarica.ContactActivity;
+import com.prachi.esoftwarica.Contacts;
+import com.prachi.esoftwarica.MainActivity;
+import com.prachi.esoftwarica.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HomeFragment extends Fragment {
+
+    private HomeViewModel homeViewModel;
+   private RecyclerView recyclerView;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        homeViewModel =
+                ViewModelProviders.of(this).get(HomeViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_home, container, false);
+        final TextView textView = root.findViewById(R.id.text_home);
+        homeViewModel.getText().observe(this, new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textView.setText(s);
+            }
+        });
+
+        recyclerView=root.findViewById(R.id.recyclerview);
+
+        List<Contacts>contactsList=new ArrayList<>();
+
+        if(MainActivity.contact.isEmpty()){
+            MainActivity.contact.add(new Contacts("Salman Khan","Mumbai","male",50));
+            ContactActivity contactActivity=new ContactActivity(getContext(),MainActivity.contact);
+            recyclerView.setAdapter(contactActivity);
+            recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+
+        }
+        else {
+
+            ContactActivity contactActivity=new ContactActivity(getContext(),MainActivity.contact);
+            recyclerView.setAdapter(contactActivity);
+            recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+
+        }
+
+
+
+
+        return root;
+    }
+}
